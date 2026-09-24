b7902057b8540392570ed975b029b9475daedcf5
MokshithRao <111580947+MokshithRao@users.noreply.github.com>
2026-09-24 19:42:32 +0530

    add customer creation endpoint

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
-from database import Base, engine
+from database import Base, engine, LocalSession
+from schemas import Customer
 import models
+
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+
+@app.post("/customers")
+def add_customer(customer: Customer):
+    db = LocalSession()
+
+    new_customer = models.Customer(
+        name = customer.name,
+        phone = customer.phone,
+        email = customer.email
+    )
+
+    db.add(new_customer)
+    db.commit()
+    db.close()
+
+    return "Customer added successfully"
